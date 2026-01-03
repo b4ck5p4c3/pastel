d3f2f35ebd9ac39acb32e6e557829f9933a3749e
Dmitry Nourell <hey@flo.boo>
2026-01-03 10:21:54 +0300

    fix: keep only safe-to-serve MIME types for raw pastes

--- src/shared/utils/mime-types.ts
+++ src/shared/utils/mime-types.ts
@@ -3,12 +3,7 @@
  * @see https://www.iana.org/assignments/media-types/media-types.xhtml
  */
 export const languageToMimeType = new Map<string, string>([
-  ['css', 'text/css'],
-  ['html', 'text/html'],
-  ['javascript', 'text/javascript'],
   ['json', 'application/json'],
-  ['markdown', 'text/markdown'],
   ['sql', 'application/sql'],
-  ['xml', 'application/xml'],
   ['yaml', 'text/yaml']
 ])
